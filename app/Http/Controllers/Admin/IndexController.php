379dfc0799569903ca8d134ba9544d5897141d7e
itas <luoyangpeng1122@163.com>
2016-09-07 20:08:55 +0800

    后台首页格式调整

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Http\Controllers\Admin;
+
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Input;
@@ -7,55 +8,56 @@ use UserRepository;
 use Auth;
 
 
-
 class IndexController extends Controller
 {
 
-	public function index()
-	{
+    public function index()
+    {
 
-		return view('admin.index.index');
-	}
+        return view('admin.index.index');
+    }
 
 
-	/**
-	 * 锁屏
-	 *
-	 */
-	public function lockScreen()
-	{
-		request()->session()->put("lock",'ok');
-		$name =auth()->user()->name;
-		return view('auth.lock',['name'=>$name]);
-	}
+    /**
+     * 锁屏
+     *
+     */
+    public function lockScreen()
+    {
+        request()->session()->put("lock", 'ok');
+        $name = auth()->user()->name;
+        return view('auth.lock', ['name' => $name]);
+    }
 
-	/**
-	 * 解锁
-	 */
-	public function unlock()
-	{
+    /**
+     * 解锁
+     *
+     */
+    public function unlock()
+    {
 
-		$uid = auth()->user()->id;
-		$user_info = UserRepository::getUserInfoById($uid);
+        $uid = auth()->user()->id;
+        $user_info = UserRepository::getUserInfoById($uid);
 
-		$password = Input::get("password");
+        $password = Input::get("password");
 
-		if(Auth::attempt(['email'=>$user_info->email,'password'=>$password])){
-			request()->session()->forget("lock");
-			return redirect('/admin');
-		}else{
-			return redirect('/admin/lock');
-		}
+        if (Auth::attempt(['email' => $user_info->email, 'password' => $password])) {
+            request()->session()->forget("lock");
+            return redirect('/admin');
+        } else {
+            return redirect('/admin/lock');
+        }
 
-	}
+    }
 
-	/**
-	 * datatable 多语言
-	 * @return string|\Symfony\Component\Translation\TranslatorInterface
-	 */
+    /**
+     * datatable 多语言
+     *
+     * @return string|\Symfony\Component\Translation\TranslatorInterface
+     */
     public function dataTableI18n()
     {
 
-    	return trans('pagination.i18n');
+        return trans('pagination.i18n');
     }
 }
