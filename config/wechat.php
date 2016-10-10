975ed3287e40797beb6c259ef4f72703e168cd6a
itas <luoyangpeng1122@163.com>
2016-10-10 23:20:08 +0800

    测试微信支付

--- config/wechat.php
+++ config/wechat.php
@@ -17,9 +17,8 @@ return [
      * 账号基本信息，请从微信公众平台/开放平台获取
      */
     'app_id'  => env('WECHAT_APPID', 'wxd741c36410676bdc'),         // AppID
-    //'secret'  => env('WECHAT_SECRET', 'a89dbc1bb0567e05642794a469ae110e'),     // AppSecret
-    'secret'  => env('WECHAT_SECRET', 'f01c21e7cf84f2147861c59620ebf1b5'),
-    'token'   => env('WECHAT_TOKEN', 'abc'),          // Token
+    'secret'  => env('WECHAT_SECRET', 'f01c21e7cf84f2147861c59620ebf1b5'),     // AppSecret
+    'token'   => env('WECHAT_TOKEN', 'ioCnU4JZqcgw5sjqHc'),          // Token
     'aes_key' => env('WECHAT_AES_KEY', ''),                    // EncodingAESKey
 
     /*
@@ -48,7 +47,7 @@ return [
     /*
      * 微信支付
      */
-    /*'payment' => [
+    'payment' => [
         'merchant_id'        => env('WECHAT_PAYMENT_MERCHANT_ID', 'your-mch-id'),
         'key'                => env('WECHAT_PAYMENT_KEY', 'key-for-signature'),
         'cert_path'          => env('WECHAT_PAYMENT_CERT_PATH', 'path/to/your/cert.pem'), // XXX: 绝对路径！！！！
@@ -57,5 +56,5 @@ return [
         // 'sub_app_id'      => env('WECHAT_PAYMENT_SUB_APP_ID', ''),
         // 'sub_merchant_id' => env('WECHAT_PAYMENT_SUB_MERCHANT_ID', ''),
 
-    ],*/
+    ],
 ];
