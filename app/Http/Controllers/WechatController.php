d34ce087f8e2c134116d35400fe3d0fde2ea1f61
itas <luoyangpeng1122@163.com>
2016-10-11 20:58:56 +0800

    微信支付

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use EasyWeChat\Foundation\Application;
 use EasyWeChat\Payment\Order;
+use App\Repositories\orderRepository;
 
 class WechatController extends Controller
 {
@@ -102,13 +103,68 @@ class WechatController extends Controller
 
         $order = new Order($attributes);
 
+
+
         $result = $payment->prepare($order);
         
         $prepayId = $result->prepay_id;
 
+        //创建数据库订单
+        $data = [
+            'goods_name' => $goods_name,
+            'price' => $price,
+             'order_number' => $order_number,
+             'transaction_id' => $prepayId,
+             'status' => 0,
+            'openid' => $user->id,
+        ];
+
+        $orderRepository = new orderRepository();
+        $orderRepository->store($data);
+
         $json = $payment->configForPayment($prepayId);
 
         return view("web.wechat.pay",compact('json','goods_name','price','company_name'));
+    }
+
+
+    /**
+     * 微信支付回调
+     * @itas
+     * @DateTime 2016-10-11
+     * @return   function   [description]
+     */
+    public function callback()
+    {
+        $response = $app->payment->handleNotify(function($notify, $successful){
+            $orderRepository = new orderRepository();
+            $order = orderRepository->findOrderByTransId($notify->transaction_id);
+
+            if (!$order) { // 如果订单不存在
+                return 'Order not exist.'; // 告诉微信，我已经处理完了，订单没找到，别再通知我了
+            }
+
+            // 如果订单存在
+            // 检查订单是否已经更新过支付状态
+            if ($order->pay_at) { // 假设订单字段“支付时间”不为空代表已经支付
+                return true; // 已经支付成功了就不再更新了
+            }
+
+            // 用户是否支付成功
+            if ($successful) {
+                // 不是已经支付状态则修改为已经支付状态
+                $order->pay_at = time(); // 更新支付时间为当前时间
+                $order->status = 1;
+            } else { // 用户支付失败
+                $order->status = -1;
+            }
+
+            $order->save(); // 保存订单
+
+            return true; // 返回处理完成
+        });
+
+        return $response;
     }
 
 
