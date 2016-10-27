ade3e13f000c9bd8c0c4fa026eb50d41f89bfe2e
itas <luoyangpeng1122@163.com>
2016-10-27 19:44:51 +0800

    id加密增加配置文件

---
+++ config/id_encrypt.php
@@ -0,0 +1,15 @@
+<?
+
+return [
+
+	'iii' => env('III','1'),
+	'kkk' => env('KKK','2'),
+	'mmm' => env('MMM','12345678'),
+	'nnn' => env('NNN','9'),
+	'ooo' => env('OOO','b'),
+	'ppp' => env('PPP','8'),
+	'qqq' => env('QQQ','b'),
+	'rrr' => env('RRR','5'),
+	'sss' => env('RRR','d'),
+
+];
