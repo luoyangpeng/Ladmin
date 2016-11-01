ebf146513b0c54028d2f1b040e90d6f41c76e926
itas <luoyangpeng1122@163.com>
2016-11-01 21:19:43 +0800

    增加表迁移文件

---
+++ database/migrations/2016_11_01_210256_create_wechat_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWechatUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('wechat_user', function (Blueprint $table) {
+            $table->string("openid",100)->comment("微信openid");
+            $table->integer("uid")->comment('绑定用id');
+            $table->string("nickname",50)->comment("昵称");
+            $table->tinyInteger("sex")->comment("0：女 1：男");
+            $table->string("province",20)->comment("省份");
+            $table->string("city",20)->comment("城市");
+            $table->string("country",50)->comment("国家");
+            $table->string("headimgurl",100)->comment("头像");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('wechat_user');
+    }
+}
---
+++ database/migrations/2016_11_01_210338_create_order_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order', function (Blueprint $table) {
+            $table->$table->increments('id');
+            $table->string("goods_name")->comment("商品名称");
+            $table->string("openid")->comment("微信用户openid");
+            $table->decimal("price")->comment("价格");
+            $table->string("order_number",50)->comment('订单号');
+            $table->string("prepay_id",100)->comment('交易号');
+            $table->tinyInteger("支付状态")->comment("支付状态 0：未支付 1已支付 -1支付失败");
+            $table->timestamp("pay_at")->comment("支付时间");
+            $table->string("from")->comment("订单来源");
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('order');
+
+    }
+}
