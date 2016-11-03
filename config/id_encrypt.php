f31efa2cad34b8654f4b0d69e95a53de640c1465
itas <luoyangpeng1122@163.com>
2016-11-03 21:05:16 +0800

    文章id加密

--- config/id_encrypt.php
+++ config/id_encrypt.php
@@ -2,14 +2,14 @@
 
 return [
 
-	'iii' => env('III','1'),
+	'iii' => env('III','5'),
 	'kkk' => env('KKK','2'),
-	'mmm' => env('MMM','12345678'),
+	'mmm' => env('MMM','123456789'),
 	'nnn' => env('NNN','9'),
 	'ooo' => env('OOO','b'),
 	'ppp' => env('PPP','8'),
-	'qqq' => env('QQQ','b'),
+	'qqq' => env('QQQ','c'),
 	'rrr' => env('RRR','5'),
-	'sss' => env('RRR','d'),
+	'sss' => env('SSS','d'),
 
 ];
