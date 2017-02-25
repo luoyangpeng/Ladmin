5d8af9fa3f4f0407c63d86a246f6c9e7442e32c6
itas <luoyangpeng1122@163.com>
2017-02-25 14:22:44 +0800

    tencent template

---
+++ public/Tencent/js/redirect.js
@@ -0,0 +1,32 @@
+function removeXSS(s) {
+	var pattern = new RegExp("[<>’”]")
+	var rs = "";
+	for (var i = 0; i < s.length; i++) {
+		rs = rs+s.substr(i, 1).replace(pattern, '');
+	}
+	return rs;
+}
+
+if (location.protocol == 'http:'){
+	//location.protocol = 'https:';
+}
+
+if (bowser.mobile) {
+	var url = location.href.replace(/(zh-cn|zh-hk|en-us)/,'mobile/$1');
+	url = removeXSS(url);
+	url = url.replace(/(company|culture|system|investor)\.html/, function($1){
+		var names = ['company.html', 'culture.html', 'system.html', 'investor.html'];
+		var count = names.length;
+		var hash = 0;
+		for(var i = 0; i < count; i++){
+			if(names[i] === $1){
+				hash = i;
+				break;
+			}
+		}
+		hash++;
+		return 'index.html#' + hash;
+	});
+	url = removeXSS(url);
+	location.href = url;
+}
