d617ba412a8b16c56abc25ab6f887685906039bb
itas <luoyangpeng1122@163.com>
2017-06-17 14:56:34 +0800

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('inspire')
-                 ->hourly();
+                 ->everyMinute();
     }
 }
